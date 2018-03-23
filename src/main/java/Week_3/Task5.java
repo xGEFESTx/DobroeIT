4debf11324a7c11ff7202812bcaa8dfd93e8ea7f
Gennadii Shuliarenko <33375003+xGEFESTx@users.noreply.github.com>
2018-03-23 14:12:44 +0200

    change Week_3 add Task#5

---
+++ src/main/java/Week_3/Task5.java
@@ -0,0 +1,43 @@
+package Week_3;
+
+public class Task5 {
+
+    public static int[] sameArray(int[] array) {
+        int max = array[0];
+        int min = array[0];
+
+        for (int i = 0; i < array.length; i++){
+            if(array[i] > max)
+                max = array[i];
+
+            if(array[i] < min)
+                min = array[i];
+        }
+
+        if (min < 0){
+            System.out.println("Введенное число меньше 0");
+            return  array;
+        }
+
+        if  (max > 100){
+            System.out.println("Введенное число больше 100");
+            return array;
+        }
+
+        int arr[] = new int[max+1];
+
+            for (int number = 0; number < array.length; number++) {
+                int count = 0;
+                for (int num = 0; num < array.length; num++) {
+
+                    if (array[number] == array[num]) {
+                        count++;
+                    }
+                }
+                int num = array[number];
+                arr[num] = count;
+            }
+
+        return arr;
+    }
+}
