741f026669dfbaae61e841b06ed025f380750593
Gennadii Shuliarenko <33375003+xGEFESTx@users.noreply.github.com>
2018-03-30 12:33:49 +0300

    Update Fish.java
    add Task#5

--- src/main/java/Week_4/Fish.java
+++ src/main/java/Week_4/Fish.java
@@ -4,8 +4,8 @@ public class Fish extends Animal {
     public Fish(double age, String name) {
         super(age, name);
     }
-    public void voice() throws NotImplementedException{
-
+    public void voice() throws NotImplementedException {
+        throw new NotImplementedException();
     }
 
 }
